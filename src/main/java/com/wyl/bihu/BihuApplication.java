4de69a450820702b0a9fabc153711c56b17bdd52
wuyl <843794313@qq.com>
2017-06-22 17:19:26 +0800

    init

---
+++ src/main/java/com/wyl/bihu/BihuApplication.java
@@ -0,0 +1,12 @@
+package com.wyl.bihu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BihuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BihuApplication.class, args);
+	}
+}
